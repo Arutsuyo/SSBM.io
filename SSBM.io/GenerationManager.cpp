7e6104ba9febe2b04b19b41a07cae8a553cdb7a1
Nara <arutsuyo@gmail.com>
2019-06-11 02:58:06 -0700

    CULL THE WEAK

--- SSBM.io/GenerationManager.cpp
+++ SSBM.io/GenerationManager.cpp
@@ -5,6 +5,15 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <conio.h>
+
+// Used in culling
+typedef std::pair<int, int> score;
 
 int GenerationManager::childCount = -1;
 int GenerationManager::childDirCount = -1;
@@ -76,21 +85,139 @@ bool GenerationManager::CullTheWeak()
     }
     std::unique_lock<std::mutex> lk(mut);
 
-    // Warn the user we are going to cull, don't kill the program
+    // Warn the user we are going to cull, don't kill the program, and yes, it will be LOUD
+    char buff[256];
+    memset(buff, 0, 256);
+    sprintf(buff, "%s:%d\tWARNING::%s IS PERFORMING A CULLING, DO NOT CLOSE PROGRAM\N", FILENM, __LINE__, FILENM);
+    printf(buff); printf(buff); printf(buff); printf(buff); printf(buff);
+    printf(buff); printf(buff); printf(buff); printf(buff); printf(buff);
+    fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff);
+    fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff); fprintf(stderr, buff);
 
-    // Initiate culling
+    // Get the top SIZE_CULL_TO
+    //                    ID   Score
+    std::vector<score> top8;
+    int val;
+    for (int i = 0; i < SIZE_GEN; i++)
+    {
+        // Read the Child Scores
+        sprintf(buff, "%s%d.h5.txt", curChildDir, i++);
+        std::fstream fs;
+        fs.open(buff, std::fstream::in);
+        if (fs.fail())
+        {
+            fprintf(stderr, "%s:%d --Error: Could not open score: %s\n", 
+                FILENM, __LINE__, buff);
+            exit(EXIT_FAILURE);
+        }
+        fs >> val;
+        fs.close();
+
+        if (top8.size() < SIZE_CULL_TO)
+        {
+            top8.push_back(score(i, val));
+            continue;
+        }
+
+        for (unsigned int j = 0; j < SIZE_CULL_TO; j++)
+        {
+            if (val > top8[j].second)
+            {
+                top8[j].first = i;
+                top8[j].second = val;
+                break;
+            }
+        }
+    }
+
+    printf("%s:%d\tIdentified Top %d\n",
+        FILENM, __LINE__, SIZE_CULL_TO);
 
     // Make Parent Dir
+    parentCount++;
+    curParentDir = DIR_AI_BASE;
+    curParentDir += modelPath + DIR_PARENT_BASE;
+    sprintf(buff, curParentDir.c_str(), parentCount);
+    curParentDir = buff;
+    sprintf(buff, "mkdir %s", curParentDir.c_str());
+    system(buff);
 
-    // Copy Children into parent
+    // Copy top SIZE_CULL_TO into parent
+    for (int i = 0; i < SIZE_CULL_TO; i++)
+    {
+        // Parse the child file based on the model path, dirCount, and ChildCount
+        char childName[256];
+        memset(childName, 0, 256);
+        sprintf(childName, "%s%d.h5", curChildDir.c_str(), top8[i].first);
 
+        // Copy the child to the parent
+        sprintf(buff, "cp %s %s%d.h5", childName, curParentDir, i);
+        system(buff);
+        sprintf(buff, "cp %s.txt %s%d.h5.txt", childName, curParentDir, i);
+        system(buff);
+    }
+
+    printf("%s:%d\tCopied to new parent dir\n",
+        FILENM, __LINE__);
     // Make new child dir
+    childDirCount++;
 
+    // Parse the Next Child Folder
+    std::string temp = DIR_AI_BASE;
+    temp += modelPath + DIR_CHILD_BASE;
+    sprintf(buff, temp.c_str(), childDirCount);
+    curChildDir = buff;
+    sprintf(buff, "mkdir %s", curChildDir.c_str());
+    system(buff);
+
+
+    printf("%s:%d\tReset Child Count\n",
+        FILENM, __LINE__);
     // Make sure variables are set right
+    childCount = -1;
 
     // Ask the user if they want to start on the next generation
     // This should be a timed prompt, if there's no response, start training
-    
+    printf("%s:%d\tAnswer the following in 10 seconds:\n",
+        FILENM, __LINE__); 
+    printf("%s:%d\tDo you want to Quit: (y or [N]) ",
+        FILENM, __LINE__);
+    std::chrono::time_point<std::chrono::high_resolution_clock> n = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed;
+    std::string response = "";
+    int dot = 1;
+    do 
+    {
+        elapsed =
+            std::chrono::high_resolution_clock::now()
+            - n;
+        if ((10 - elapsed.count()) / dot >= 1)
+        {
+            dot++;
+            printf("%d ", (10-elapsed.count()));
+        }
+
+        if (kbhit())
+        {
+            std::cin >> response;
+            break;
+        }
+    } while (elapsed.count() > 10);
+
+    if (response.size())
+    {
+        // Check for Response
+        if (response.find("y") == std::string::npos
+            || response.find("q") == std::string::npos)
+        {
+            printf("%s:%d\tQuitting . . .\n",
+                FILENM, __LINE__);
+            return false;
+        }
+    }
+    printf("%s:%d\tContinuing . . .\n",
+        FILENM, __LINE__);
+    return true;
 }
 
 bool GenerationManager::Initialize(std::string model_path)
